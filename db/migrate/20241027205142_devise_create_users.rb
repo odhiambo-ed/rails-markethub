fee9caac2710f2f50e3606b7b0d26cdf9c109fc5
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-10-27 23:54:15 +0300

    edit migration

--- db/migrate/20241027205142_devise_create_users.rb
+++ db/migrate/20241027205142_devise_create_users.rb
@@ -14,6 +14,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Rememberable
       t.datetime :remember_created_at
 
+      # custom
+      t.text :address
+
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
