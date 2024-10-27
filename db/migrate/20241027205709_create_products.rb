117aaf5b20589f1c72402f2b0f1db611789dc2b1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-10-28 00:01:11 +0300

    edit migration

--- db/migrate/20241027205709_create_products.rb
+++ db/migrate/20241027205709_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[7.0]
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.decimal :price
+      t.decimal :price, precision: 5, scale: 2
 
       t.timestamps
     end
